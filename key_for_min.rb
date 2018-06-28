68a2654121c13177ec29e5fce09cd40182c6a44f
jmoypace <jmoypace@gmail.com>
2018-06-28 17:42:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
+
 
 def key_for_min_value(name_hash)
 arr=nil
@@ -8,7 +8,7 @@ name_hash.collect do |kids_name, age|
   arr=age
 end
 
-puts arr
-binding.pry
+name_hash[arr]
+
 
 end
