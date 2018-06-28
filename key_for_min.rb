a9fed90455fb2ff06d1c9168c5bf01f8e2bc5bb6
jmoypace <jmoypace@gmail.com>
2018-06-28 17:27:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-ages=new.array
+
 name_hash.collect do |kids_name, age|
-  ages=age
+
 end
 
-if(ages[0]>ages[1])
-  lowest=ages[1]
-elsif (ages[1]>ages[2])
-  lowest=ages[2]
-else
-  lowest=ages[0]
-end
 
-name_hash[lowest]
 end
