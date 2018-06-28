3b965ef9e68c29017ab18a45e0b45ddf53085b6a
jmoypace <jmoypace@gmail.com>
2018-06-28 18:04:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,25 @@
 
 
 def key_for_min_value(name_hash)
-
+newarr=[]
+newarrnames=[]
 name_hash.collect do |kids_name, age|
-age  
+newarr.push(age)
+newarrnames.push(kids_name)
+end
+if(newarr[0]>newarr[1])
+lowest=newarr[1]
+end
+if
+newarr[1]>newarr[2]
+lowest=newarr[2]
+
+else
+lowest=newarr[0]
+end
+holder=newarr.index(lowest)
+newarrnames[holder]
+
 end
 
 
