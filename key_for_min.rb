fbeb0758aa22c9e70864e9a247d59297733bad1a
jmoypace <jmoypace@gmail.com>
2018-06-28 17:24:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,4 +6,12 @@ def key_for_min_value(name_hash)
 name_hash.collect do |kids_name, age|
   age
 end
+if(age[0]>age[1])
+  lowest=age[1]
+elsif (age[1]>age[2])
+  lowest=age[2]
+else
+  lowest=age[0]
+end
+name_hash[lowest]
 end
