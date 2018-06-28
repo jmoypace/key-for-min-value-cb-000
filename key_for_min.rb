6c61b87ef90999d31a13796ba9a24a70406d5cd1
jmoypace <jmoypace@gmail.com>
2018-06-28 17:21:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  ages=nil
-name_hash.each do |kids_name, age|
-  if ages==nil
-ages=age
-puts ages
-  elsif ages < age
-    ages=age
+  
+name_hash.collect do |kids_name, age|
+  age
 end
-end
-name_hash[ages]
 end
